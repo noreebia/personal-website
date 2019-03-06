c85714878c7eb445b6e37c7fd2a88b294396b8d2
noreebia <noreebia@gmail.com>
2019-03-06 17:40:07 +0900

    Modify default components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <header className="banner">
+          <a href="/">
+            <img id="banner" src="http://demo.qodeinteractive.com/central/wp-content/uploads/2013/05/header.jpg" alt="banner" href="/"></img>
           </a>
         </header>
+        <nav>
+          <ul id="navigation-bar">
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/blog/tech">Tech</a>
+            </li>
+            <li>
+              <a href="/blog/tech">Personal</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
